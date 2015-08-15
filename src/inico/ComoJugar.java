eb0c32467e4e7a99d71046aa617a058ad91fdf21
Miguel <Miguel@Mick-PC>
2015-08-15 18:26:31 -0500

    Profesor aqui esta mi juego

---
+++ src/inico/ComoJugar.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inico;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Dark
+ */
+public class ComoJugar {
+    
+    public int[] getCardNumbers() {
+        
+        int[] numbers = new int[16];
+        int con = 0;
+        
+        while(con < 16) {
+            Random a = new Random();
+            int ice = a.nextInt(8) + 1;
+            int b = 0;
+            
+            for (int i = 0; i < 16; i++) {
+                if(numbers[i] == ice) {
+                    b++;
+                }
+            }
+            if(b < 2) {
+                numbers[con] = ice;
+                con++;
+            }//fin
+            
+        }
+        
+        
+        return numbers;
+    }
+            
+}
